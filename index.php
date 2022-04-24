1a05baad2d4d3c3c1932f25e905d4b31ff0efe3b
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-04-24 18:46:32 +0200

    glassmorphism container

--- index.php
+++ index.php
@@ -6,27 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TristanBonnal-Dev</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400&family=Nunito:ital,wght@0,300;0,400;0,600;0,700;1,400&family=Quicksand:wght@300;400;500;700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <h1><a href="#"> Logo</a></h1>
-        <nav>
-            <ul>
-                <li><a href="/">Accueil</a></li>
-                <li><a href="/">Profil</a></li>
-                <li><a href="/">Réalisations</a></li>
-                <li><a href="/">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti fuga eos ipsum exercitationem labore consequatur quisquam unde perferendis provident. Quas aperiam quod ea quo quasi distinctio earum itaque nemo nulla.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti fuga eos ipsum exercitationem labore consequatur quisquam unde perferendis provident. Quas aperiam quod ea quo quasi distinctio earum itaque nemo nulla.
-        </p>
-    </main>
-    <footer>
-        footer
-    </footer>
+    <div class="container">
+        <header class="ban">
+            <h1><a href="#">Tristan Bonnal</a></h1>
+            <nav>
+                <ul class="nav">
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="/">Profil</a></li>
+                    <li><a href="/">Réalisations</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti fuga eos ipsum exercitationem labore consequatur quisquam unde perferendis provident. Quas aperiam quod ea quo quasi distinctio earum itaque nemo nulla.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti fuga eos ipsum exercitationem labore consequatur quisquam unde perferendis provident. Quas aperiam quod ea quo quasi distinctio earum itaque nemo nulla.
+            </p>
+        </main>
+        <footer>
+            footer
+        </footer>
+    </div>
 </body>
 </html>
