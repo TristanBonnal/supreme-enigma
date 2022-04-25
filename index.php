78b41bdd0259f54c6f2e22ed0908ca9a568896c0
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-04-25 15:38:38 +0200

    new start

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="circle"></div>
-    <div class="circle"></div>
     <div class="container">
         <header class="ban">
             <h1 class="logo"><a href="#"><span class="uppercase">T</span>ristan <span class="uppercase">B</span>onnal</a></h1>
