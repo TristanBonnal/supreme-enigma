7158a027d4dd5ad40621e4170187df4afbde1568
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-05-12 21:37:10 +0200

    switch theme ok

--- public/assets/js/switchBtn.js
+++ public/assets/js/switchBtn.js
@@ -1,16 +1,34 @@
 const switchBtn = {
     init: function() {
-        console.log('hello');
         const switchButton = document.getElementById('switch');
-        switchButton.addEventListener('click', switchBtn.switchLight)
+        switchButton.addEventListener('click', switchBtn.switchTheme);
+        const theme = localStorage.getItem('theme');
+        if (theme == 'light') {
+            switchBtn.addLight();
+        }
 
     },
+    switchTheme: function() {
+        const theme = localStorage.getItem('theme');
+        if (theme == 'light') {
+            switchBtn.removeLight();
+            localStorage.setItem('theme','dark')
+        } else if (theme == 'dark') {
+            switchBtn.addLight();
+            localStorage.setItem('theme','light')
+        }
+    },
 
-    switchLight: function() {
-        console.log('clic');
-        document.getElementById('switch').classList.toggle('light');
-        document.querySelector(':root').classList.toggle('light');
-        document.querySelector('.header').classList.toggle('light');
+    addLight: function() {
+        document.getElementById('switch').classList.add('light');
+        document.querySelector(':root').classList.add('light');
+        document.querySelector('.header').classList.add('light');
+    },
+
+    removeLight: function() {
+        document.getElementById('switch').classList.remove('light');
+        document.querySelector(':root').classList.remove('light');
+        document.querySelector('.header').classList.remove('light');
     }
 }
 
